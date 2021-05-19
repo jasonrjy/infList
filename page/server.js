ab1b652a00323dfefe87d74674270b92cee2f527
0rdinary <ac980925@gmail.com>
2021-05-19 20:55:19 +0900

    index page bug fixed

--- page/server.js
+++ page/server.js
@@ -4,9 +4,21 @@ const fs = require('fs');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.get('/*', (req, res, next) => {
+app.use(express.static('app'))
+app.use(express.static('img'))
+app.use(express.static('css'))
+app.use(express.static('js'))
+
+app.get('/', (req, res, next) => {
   const indexPage = fs.readFileSync('./index.html');
   return res.status(200).send(indexPage.toString());
+});
+
+app.get('/main', function(req, res) {
+  fs.readFile('html/main.html', function(error, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+  });
 });
 
 app.use((req, res, next) => {
